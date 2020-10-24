60e7789787b34bb52b8bce60c9fb3075375734eb
YiXiongyu <1651960291@qq.com>
2020-10-24 17:41:36 +0800

    页面结构示例完成

---
+++ src/components/home/Comments.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="comments">
+    {{ comment }}
+  </div>
+</template>
+<script>
+export default {
+  name: "comments-home",
+  data() {
+    return {
+      comment: "这里是用户评论区",
+    };
+  },
+};
+</script>
+<style lang="less" scoped>
+.comments {
+  background: white;
+  height: 1000px;
+  width: 1000px;
+}
+</style>
---
+++ src/views/Edit.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{info}}
+  </div>
+</template>
+
+<script>
+export default {
+  name:"edit",
+  data(){
+    return {
+      info:"这里放编辑组件"
+    }
+  }
+}
+</script>
