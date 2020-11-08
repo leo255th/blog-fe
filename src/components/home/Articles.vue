1443c84c35c4de671bf188881029d65160d95871
YiXiongyu <leoneyi@qq.com>
2020-11-08 16:02:51 +0800

    添加组件参数类型约束

--- src/components/home/Articles.vue
+++ src/components/home/Articles.vue
@@ -4,29 +4,36 @@
     <button @click="login(loginRes)">测试登陆</button>
   </div>
 </template>
-<script>
+<script lang='ts'>
 import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+import { ArticleList, ByPwdInput } from "../../store/gql/types";
+
+// 用TS类型约束组件参数类型
+type dataType = {
+  article: string;
+  loginInput: ByPwdInput;
+};
 export default {
   name: "articles-home",
   data() {
     return {
       article: "这里是近期文章列表",
-      loginRes:{
-        userName:'用户1号',
-        pwd:'abc123'
-      }
-    };
+      loginInput: {
+        userName: "用户1号",
+        pwd: "abc123",
+      },
+    } as dataType;
   },
-  methods:{
-    ...mapActions('user',{
-      login:'login'
-    })
-  }
+  methods: {
+    ...mapActions("user", {
+      login: "login",
+    }),
+  },
 };
 </script>
 <style lang="less" scoped>
 .articles {
-    background: white;
+  background: white;
   height: 400px;
   width: 1000px;
 }
